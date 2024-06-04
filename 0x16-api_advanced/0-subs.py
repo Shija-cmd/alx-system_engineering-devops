307ac075beae628a7a865a951e612b7984d5e614
Shija-cmd <shijajuma@protonmail.com>
2024-06-04 20:03:19 +0000

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and returns the number of subscribers 
+queries the Reddit API and returns the number of subscribers (not active
+users, total subscribers) for a given subreddit 
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    queries the Reddit API and returns the number of subscribers 
+    queries the Reddit API and returns the number of subscribers (not active
+	users, total subscribers) for a given subreddit 
     """
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 0)'}
